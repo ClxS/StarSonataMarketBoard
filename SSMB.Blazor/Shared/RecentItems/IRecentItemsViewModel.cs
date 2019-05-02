1debf7c372a824f3ad09de287b8be79f1c2d62f6
Carl <slxxls92@gmail.com>
2019-05-02 22:00:05 +0100

    Moved RecentItemsViewModel into it's own file

--- SSMB.Blazor/Shared/RecentItems/IRecentItemsViewModel.cs
+++ SSMB.Blazor/Shared/RecentItems/IRecentItemsViewModel.cs
@@ -1,40 +1,13 @@
 ﻿namespace SSMB.Blazor.Shared.RecentItems
 {
     using System;
-    using System.Reactive.Linq;
-    using System.Reactive.Subjects;
     using Application.Items.Models;
     using Domain;
-    using ViewServices;
 
     public interface IRecentItemsViewModel
     {
         RecentItem[] Results { get; }
 
         IObservable<RecentItem[]> WhenResultsChanged { get; }
-    }
-
-    class RecentItemsViewModel : IRecentItemsViewModel
-    {
-        private readonly Subject<RecentItem[]> resultsChangedSubject;
-        private RecentItem[] results;
-
-        public RecentItemsViewModel(IItemsService itemsService)
-        {
-            this.resultsChangedSubject = new Subject<RecentItem[]>();
-            itemsService.GetRecentlyUpdatedItems().ContinueWith(items => { this.Results = items.Result; });
-        }
-
-        public RecentItem[] Results
-        {
-            get => this.results;
-            set
-            {
-                this.results = value;
-                this.resultsChangedSubject.OnNext(value);
-            }
-        }
-
-        public IObservable<RecentItem[]> WhenResultsChanged => this.resultsChangedSubject.AsObservable();
     }
 }
---
+++ SSMB.Blazor/Shared/RecentItems/RecentItemsViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace SSMB.Blazor.Shared.RecentItems
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+    using Application.Items.Models;
+    using ViewServices;
+
+    class RecentItemsViewModel : IRecentItemsViewModel
+    {
+        private readonly Subject<RecentItem[]> resultsChangedSubject;
+        private RecentItem[] results;
+
+        public RecentItemsViewModel(IItemsService itemsService)
+        {
+            this.resultsChangedSubject = new Subject<RecentItem[]>();
+            itemsService.GetRecentlyUpdatedItems().ContinueWith(items => { this.Results = items.Result; });
+        }
+
+        public RecentItem[] Results
+        {
+            get => this.results;
+            set
+            {
+                this.results = value;
+                this.resultsChangedSubject.OnNext(value);
+            }
+        }
+
+        public IObservable<RecentItem[]> WhenResultsChanged => this.resultsChangedSubject.AsObservable();
+    }
+}
