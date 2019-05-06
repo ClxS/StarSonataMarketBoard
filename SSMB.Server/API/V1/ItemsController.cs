6c8044f109f570d417c5586cf68cfbe958d8df76
Carl <slxxls92@gmail.com>
2019-05-06 22:55:46 +0100

    Added API controller method for getting an item's details

--- SSMB.Server/API/V1/ItemsController.cs
+++ SSMB.Server/API/V1/ItemsController.cs
@@ -6,6 +6,7 @@
     using System.Web.Http;
     using Application.Interfaces;
     using Application.Items.Models;
+    using Application.Items.Queries.GetFullItemDetails;
     using Application.Items.Queries.GetHotItems;
     using Application.Items.Queries.GetItems;
     using Application.Items.Queries.GetRecentItems;
@@ -27,6 +28,12 @@
         public async Task<IEnumerable<Item>> Index([FromQuery] string filter, [FromQuery] int count = 20)
         {
             return await this.mediator.Send(new GetItemsQuery { Filter = filter, Count = count });
+        }
+
+        [System.Web.Http.HttpGet]
+        public async Task<FullDetailItem> Detail([FromQuery] int id)
+        {
+            return await this.mediator.Send(new GetFullItemDetailsQuery { ItemId = id });
         }
 
         [System.Web.Http.HttpGet]
