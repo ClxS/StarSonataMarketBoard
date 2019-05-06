3c6d1fdc5d5fdfad0b132aeba280c018da843da6
Carl <slxxls92@gmail.com>
2019-05-06 22:53:28 +0100

    Missing model from previous commit

---
+++ SSMB.Application/Items/Models/FullDetailItem.cs
@@ -0,0 +1,17 @@
+﻿namespace SSMB.Application.Items.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using Domain;
+
+    public class FullDetailItem
+    {
+        public Item Item;
+
+        public OrderBatch LatestOrder;
+
+        public List<(DateTime date, long price, long quantity)> PurchasePrices { get; set; }
+
+        public List<(DateTime date, long price, long quantity)> SalePrices { get; set; }
+    }
+}
