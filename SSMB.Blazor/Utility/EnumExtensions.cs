476126f59a4a12827f93943783acf2234ff2180a
Carl <slxxls92@gmail.com>
2019-05-12 22:46:35 +0100

    Added ToDescriptiion extension to convert an enum into it's description attribute

---
+++ SSMB.Blazor/Utility/EnumExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace SSMB.Blazor.Utility
+{
+    using System;
+    using System.ComponentModel;
+
+    public static class EnumExtensions
+    {
+        public static string ToDescription(this Enum @this)
+        {
+            var type = @this.GetType();
+
+            var memInfo = type.GetMember(@this.ToString());
+
+            if (memInfo == null || memInfo.Length <= 0)
+            {
+                return @this.ToString();
+            }
+
+            var attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attrs != null && attrs.Length > 0)
+            {
+                return ((DescriptionAttribute)attrs[0]).Description;
+            }
+
+            return @this.ToString();
+        }
+    }
+}
