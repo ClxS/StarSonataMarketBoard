b463d2638416286c20aa32c895cbf64d59eff38a
Carl <slxxls92@gmail.com>
2019-07-24 20:32:24 +0100

    Re-enabled FindItemsToCheckExist

--- SSMB.DataCollection/Jobs/FindItemsToCheckExist.cs
+++ SSMB.DataCollection/Jobs/FindItemsToCheckExist.cs
@@ -27,7 +27,6 @@
         [Queue("gather_checkable")]
         public async Task DoFindItemsToCheckExist()
         {
-            return;
             Console.WriteLine("Starting Job: FindItemsToCheckExist");
 
             using var dbContext = this.dbContextFactory();
